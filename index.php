cda4f28f03eec0ff2581d3f3a2869554ec7bd76c
MAXON MACHEDA MR 200 <bsc-com-ne-12-18@cc.ac.mw>
2022-07-27 00:37:23 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <Link rel="stylesheet" type ="text/css" href="css/style.css" >
+    <title>Macheda</title>
+</head>
+<body>
+    <div class="container">
+  
+    <h1 class= "max">Macheda Graphics</h1>
+    
+
+
+    <div id="navbar">
+      
+     <ul  >
+        <li><a href="#">Home</a></li> 
+        <li><a href="#">Services</a></li> 
+        <li><a href="#">About Us</a></li> 
+        <li><a href="#">Networking</a></li> 
+        <li><a href="#">Contact Us</a></li> 
+
+
+
+
+     </ul>
+</div>
+     </div>
+
+</body>
+</html>
